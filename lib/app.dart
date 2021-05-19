1617b4134a681e3751aad3989464888d427b2a4e
Imali Susan <susanimali52@gmail.com>
2021-05-18 19:15:54 -0700

    Add navigation

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'screens/location_detail/location_detail.dart';
+import 'screens/locations/locations.dart';
 import 'style.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LocationDetail(),
+      home: Locations(),
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           textTheme: TextTheme(headline6: AppBarTextStyle)),
--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,19 +1,39 @@
 import 'location_fact.dart';
 
 class Location {
+  final int id;
   String name;
   String imagePath;
   final List<LocationFact> facts;
 
-  Location(this.name, this.imagePath, this.facts);
+  Location(this.id, this.name, this.imagePath, this.facts);
 
-  static List<Location> fetchAll()
-  {
-    return[
-      Location('Mombasa Kenya', 'assets/images/image.png', [
-        LocationFact('Fun Fact 1', 'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful'),
-        LocationFact('Fun Fact 2', 'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful '),
-      ])
+  static List<Location> fetchAll() {
+    return [
+      Location(1, 'Amboseli National Reserve', 'assets/images/image3.png', [
+        LocationFact('Fun Fact 1',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful'),
+        LocationFact('Fun Fact 2',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful '),
+      ]),
+       Location(2, 'Maasai Mara', 'assets/images/image4.png', [
+        LocationFact('Fun Fact 1',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful'),
+        LocationFact('Fun Fact 2',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful '),
+      ]),
+       Location(3, 'Tsavo National Park', 'assets/images/image2.png', [
+        LocationFact('Fun Fact 1',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful'),
+        LocationFact('Fun Fact 2',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful '),
+      ]),
+       Location(4, 'Mt Kilimanjaro', 'assets/images/image5.png', [
+        LocationFact('Fun Fact 1',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful'),
+        LocationFact('Fun Fact 2',
+            'Google Fonts is a library of 1052 free licensed font families and APIs for conveniently using the fonts via CSS and Android. We also provide delightful '),
+      ]),
     ];
   }
 }
